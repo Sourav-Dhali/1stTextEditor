39e54efe7e25931b22a94f44e463b7858bb2db8b
sourav <souravd2397@gmail.com>
2023-07-17 01:11:45 +0530

    toggol fix

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -3,19 +3,15 @@ import { Link } from 'react-router-dom';
 
 export default function Navbar(props) {
   return (
-    <nav className={`navbar navbar-expand-lg navbar-${props.mode} bg-${props.mode}`}>
-      <div className="container-fluid ">
-        <Link className="navbar-brand nav-title" to="/">{props.title}</Link>
-        <div className="collapse navbar-collapse " id="navbarSupportedContent">
-          <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-            
-          </ul>
-          <div className={`form-check form-switch text-${props.mode==='lighyt'?'dark':'light'}`}>
-            <input className="form-check-input" type="checkbox" onClick={props.toggleMode} role="switch" id="flexSwitchCheckDefault"/>
-              {/* <label className="form-check-label" htmlFor="flexSwitchCheckDefault">{props.modeIs}</label> */}
-          </div>
+    <>
+      <nav className={` navNav navbar-${props.mode} bg-${props.mode}`}>
+        <div >
+          <Link className="nav-title" to="/">{props.title}</Link>
         </div>
-      </div>
-    </nav>
+        <div className={`form-check form-switch text-${props.mode === 'lighyt' ? 'dark' : 'light'}`}>
+          <input className="form-check-input" type="checkbox" onClick={props.toggleMode} role="switch" id="flexSwitchCheckDefault" />
+        </div>
+      </nav>
+    </>
   )
 }
